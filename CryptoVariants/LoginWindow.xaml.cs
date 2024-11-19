b0c4a5ec96eed51abadeb2e5726622a67ae3f94a
Ilya <ilya925512@gmail.com>
2024-11-19 23:22:14 +0300

    initial

---
+++ CryptoVariants/LoginWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CryptoVariants
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text == "" || Password.Password == "")
+            {
+                MessageBox.Show("Введите данные для входа");
+            } else
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+           
+        }
+    }
+}
---
+++ CryptoVariants/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+
+namespace CryptoVariants
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            EncryptionMethodComboBox.SelectedIndex = 0;
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            string inputText = InputTextBox.Text;
+            string result = string.Empty;
+            if (inputText == "")
+            {
+                MessageBox.Show("Заполните обязательное поле.");
+            }
+
+            switch (EncryptionMethodComboBox.SelectedIndex)
+            {
+                case 0:
+                    result = CaesarCipher(inputText, 3);
+                    break;
+                case 1:
+                    result = SimpleSubstitutionCipher(inputText);
+                    break;
+                case 2:
+                    result = VigenereCipher(inputText, "KEY"); 
+                    break;
+                case 3:
+                    result = RsaEncrypt(inputText);
+                    break;
+                default:
+                    MessageBox.Show("Выберите метод шифрования.");
+                    return;
+            }
+
+            OutputTextBox.Text = result;
+        }
+
+        private string CaesarCipher(string input, int shift)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    char offset = char.IsUpper(c) ? 'A' : 'a';
+                    char encryptedChar = (char)((((c + shift) - offset + 26) % 26) + offset);
+                    result.Append(encryptedChar);
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private string SimpleSubstitutionCipher(string input)
+        {
+           
+            StringBuilder result = new StringBuilder();
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    char offset = char.IsUpper(c) ? 'A' : 'a';
+                    char encryptedChar = (char)(offset + ('z' - char.ToLower(c)));
+                    result.Append(encryptedChar);
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+            return result.ToString();
+        }
+
+        private string VigenereCipher(string input, string key)
+        {
+            StringBuilder result = new StringBuilder();
+            int keyIndex = 0;
+
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    char offset = char.IsUpper(c) ? 'A' : 'a';
+                    char keyChar = char.ToUpper(key[keyIndex % key.Length]);
+                    int shift = keyChar - 'A';
+                    char encryptedChar = (char)((((c + shift) - offset + 26) % 26) + offset);
+                    result.Append(encryptedChar);
+                    keyIndex++;
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private string RsaEncrypt(string plainText)
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+               
+                rsa.KeySize = 2048;
+                RSAParameters rsaParams = rsa.ExportParameters(true);
+
+               
+                byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+                byte[] encryptedBytes = rsa.Encrypt(plainBytes, false);
+
+                
+                return Convert.ToBase64String(encryptedBytes);
+            }
+        }
+    }
+}
